c6eb891234df54bdadf0f9cc47b80ac000c84304
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-28 15:18:03 +1000

    loading and error

--- src/App.js
+++ src/App.js
@@ -5,15 +5,34 @@ import axios from "axios";
 
 const App = () => {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const res = await axios.get('http://localhost:8080/books');
-      setBooks(res.data);
+      setError(false);
+      setLoading(true);
+
+      try {
+        const res = await axios.get('http://localhost:8080/books');
+        setBooks(res.data);
+      } catch (e) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchBooks();
   }, []);
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
+  if(error) {
+    return <p>Error...</p>
+  }
 
   return (
     <div>
