d2f788559b2580069d9e0a736ccf221ea6df18d7
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-28 13:57:23 +1000

    extract the data out

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 function App() {
+  const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
+
   return (
     <div>
       <Typography variant="h2" component="h2" data-test="heading">
       Bookish
       </Typography>
       <div data-test="book-list">
-        <div className="book-item">
-          <h2 className="title">Refactoring</h2>
-        </div>
-        <div className="book-item">
-          <h2 className="title">Domain-driven design</h2>
-        </div>
+      {
+        books.map(book => (<div className="book-item">
+          <h2 className="title">{book.name}</h2>
+        </div>))
+      }
       </div>
     </div>
   );
