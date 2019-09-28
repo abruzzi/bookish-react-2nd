a2dd77b5fc24d1108ec72b427ca75c43d158cfed
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-28 12:54:28 +1000

    booklist test

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,6 +1,17 @@
-describe('Bookish application', function() {
-  it('Visits the bookish', function() {
+describe('Bookish application', () => {
+  it('Visits the bookish', () => {
     cy.visit('http://localhost:3000/');
     cy.get('h2[data-test="heading"]').contains('Bookish')
+  })
+
+  it('Shows a book list', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('div[data-test="book-list"]').should('exist');
+    cy.get('div.book-item').should((books) => {
+      expect(books).to.have.length(2);
+
+      const titles = [...books].map(x => x.querySelector('h2').innerHTML);
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+    })
   })
 })
--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
       <Typography variant="h2" component="h2" data-test="heading">
       Bookish
       </Typography>
+      <div data-test="book-list">
+        <div className="book-item">
+          <h2 className="title">Refactoring</h2>
+        </div>
+        <div className="book-item">
+          <h2 className="title">Domain-driven design</h2>
+        </div>
+      </div>
     </div>
   );
 }
