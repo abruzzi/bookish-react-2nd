ac2dbcbea3348aea2ef068a5ecdbedeedeaa57f6
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-28 14:27:18 +1000

    axios and fetch data from json-server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import BookList from "./BookList";
+import axios from "axios";
 
-function App() {
-  const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get('http://localhost:8080/books');
+      setBooks(res.data);
+    };
+
+    fetchBooks();
+  }, []);
 
   return (
     <div>
