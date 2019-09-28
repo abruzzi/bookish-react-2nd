19a673f29671a9d71c2dc80df674dceb6e59dfa8
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-28 15:05:32 +1000

    missing parts for previous commit

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -4,8 +4,9 @@ describe('Bookish application', () => {
   beforeEach(() => {
     const books = [
       {"name": "Refactoring", "id": 1},
-      {"name": "Domain-driven design", "id": 2}
-    ]
+      {"name": "Domain-driven design", "id": 2},
+      {"name": "Building Micro-service", "id": 3}
+    ];
 
     return books.map(item => axios.post('http://localhost:8080/books', item, {headers: { 'Content-Type': 'application/json' }}))
   });
@@ -23,10 +24,10 @@ describe('Bookish application', () => {
     cy.visit('http://localhost:3000/');
     cy.get('div[data-test="book-list"]').should('exist');
     cy.get('div.book-item').should((books) => {
-      expect(books).to.have.length(2);
+      expect(books).to.have.length(3);
 
       const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Micro-service'])
     })
   });
 });
