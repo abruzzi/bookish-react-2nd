143398322d7d12b5c38c5d7986ebefe6ea88330d
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-28 20:52:39 +1000

    book description

--- src/BookDetail.jsx
+++ src/BookDetail.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const BookDetail = ({book}) => {
   return (<div className="detail">
     <h2 className="book-title">{book.name}</h2>
+    <p className="book-description">{book.description}</p>
   </div>)
 }
 
--- src/BookDetail.test.jsx
+++ src/BookDetail.test.jsx
@@ -15,5 +15,19 @@ describe('BookDetail', () => {
 
     const title = container.querySelector('.book-title');
     expect(title.innerHTML).toEqual(props.book.name);
+  });
+
+  it('renders description', () => {
+    const props = {
+      book: {
+        name: 'Refactoring',
+        description: 'Martin Fowler’s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software.'
+      }
+    };
+
+    const {container} = render(<BookDetail {...props} />);
+
+    const title = container.querySelector('p.book-description');
+    expect(title.innerHTML).toEqual(props.book.description);
   })
 });
