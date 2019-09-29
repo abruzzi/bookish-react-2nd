a41618829b260a65a946986b8329428e189548fa
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-30 06:36:30 +1000

    toggle button for review

--- src/BookDetail/Review.jsx
+++ src/BookDetail/Review.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import Button from "@material-ui/core/Button/Button";
 
-const Review = ({review}) => (<div className="review">
-  <span className="name">{review.name}</span>
-  <span className="date">{review.date}</span>
-  <p className="content">{review.content}</p>
-</div>);
+const Review = ({review}) => {
+  const [editing, setEditing] = useState(false);
+
+  return (<div className="review">
+
+    <span className="name">{review.name}</span>
+    <span className="date">{review.date}</span>
+    <p className="content">{review.content}</p>
+
+    <Button variant="contained" color="primary" name="submit" onClick={() => setEditing(!editing)}>
+      {!editing ? "Edit" : "Submit"}
+    </Button>
+  </div>)
+};
 
 export default Review;
--- src/BookDetail/Review.test.jsx
+++ src/BookDetail/Review.test.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 import Review from "./Review";
+import userEvent from "@testing-library/user-event";
 
 describe('Review', () => {
   it('renders', () => {
@@ -21,5 +22,24 @@ describe('Review', () => {
     expect(review.querySelector('.date').innerHTML).toEqual('2018/06/21');
     expect(review.querySelector('.content').innerHTML)
       .toEqual('Excellent work, really impressive on the efforts you put');
+  });
+
+  it('editing', () => {
+    const props = {
+      review: {
+        name: 'Juntao',
+        date: '2018/06/21',
+        content: 'Excellent work, really impressive on the efforts you put'
+      },
+    };
+
+    const {getByText} = render(<Review {...props}/>);
+    const button = getByText('Edit');
+
+    expect(button.innerHTML).toEqual('Edit');
+
+    userEvent.click(button);
+
+    expect(button.innerHTML).toEqual('Submit');
   });
 });
