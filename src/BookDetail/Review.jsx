829ce1a4ee6b5846e7c75df6a8089a17a8d4b01c
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-30 07:00:31 +1000

    editing

--- src/BookDetail/Review.jsx
+++ src/BookDetail/Review.jsx
@@ -1,14 +1,27 @@
 import React, {useState} from "react";
 import Button from "@material-ui/core/Button/Button";
+import TextField from "@material-ui/core/TextField/TextField";
 
 const Review = ({review}) => {
   const [editing, setEditing] = useState(false);
+  const [content, setContent] = useState(review.content);
 
   return (<div className="review">
 
     <span className="name">{review.name}</span>
     <span className="date">{review.date}</span>
-    <p className="content">{review.content}</p>
+
+    {!editing ? <p className="content">{review.content}</p> : (<TextField
+      name="content"
+      label="Content"
+      margin="normal"
+      variant="outlined"
+      multiline
+      rowsMax="4"
+      value={content}
+      onChange={e => setContent(e.target.value)}
+    />)}
+
 
     <Button variant="contained" color="primary" name="submit" onClick={() => setEditing(!editing)}>
       {!editing ? "Edit" : "Submit"}
--- src/BookDetail/Review.test.jsx
+++ src/BookDetail/Review.test.jsx
@@ -3,6 +3,7 @@ import {render} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 import Review from "./Review";
 import userEvent from "@testing-library/user-event";
+import {prettyDOM} from "@testing-library/dom";
 
 describe('Review', () => {
   it('renders', () => {
@@ -41,5 +42,31 @@ describe('Review', () => {
     userEvent.click(button);
 
     expect(button.innerHTML).toEqual('Submit');
+  });
+
+  it('copy content to a textarea for editing', () => {
+    const props = {
+      review: {
+        name: 'Juntao',
+        date: '2018/06/21',
+        content: 'Excellent work, really impressive on the efforts you put'
+      },
+    };
+
+    const {getByText, container} = render(<Review {...props}/>);
+    const button = getByText('Edit');
+    const content = container.querySelector('p.content');
+    const editingContent = container.querySelector('textarea[name="content"]');
+
+    expect(content).toBeInTheDocument();
+    expect(container.querySelector('textarea[name="content"]')).not.toBeInTheDocument();
+
+    userEvent.click(button);
+
+    expect(content).not.toBeInTheDocument();
+
+    expect(container.querySelector('textarea[name="content"]')).toBeInTheDocument();
+    expect(container.querySelector('textarea[name="content"]').innerHTML)
+      .toEqual('Excellent work, really impressive on the efforts you put');
   });
 });
