b062ac95011cef784b410bd0c2eec68a286d88be
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-29 21:51:10 +1000

    restyling

--- src/BookDetail/ReviewForm.jsx
+++ src/BookDetail/ReviewForm.jsx
@@ -3,6 +3,7 @@ import TextField from "@material-ui/core/TextField/TextField";
 import Button from "@material-ui/core/Button/Button";
 import * as actions from "../redux/actions/actions";
 import {useDispatch} from "react-redux";
+import Grid from "@material-ui/core/Grid/Grid";
 
 const ReviewForm = ({id}) => {
   const [name, setName] = useState('');
@@ -11,29 +12,37 @@ const ReviewForm = ({id}) => {
   const dispatch = useDispatch();
 
   return (<form noValidate autoComplete="off">
-    <TextField
-      label="Name"
-      name="name"
-      margin="normal"
-      variant="outlined"
-      value={name}
-      onChange={e => setName(e.target.value)}
-    />
+    <Grid container>
+      <Grid item xs={12}>
+        <TextField
+          label="Name"
+          name="name"
+          margin="normal"
+          variant="outlined"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </Grid>
 
-    <TextField
-      name="content"
-      label="Content"
-      margin="normal"
-      variant="outlined"
-      multiline
-      rowsMax="4"
-      value={content}
-      onChange={e => setContent(e.target.value)}
-    />
+      <Grid item xs={12}>
+        <TextField
+          name="content"
+          label="Content"
+          margin="normal"
+          variant="outlined"
+          multiline
+          rowsMax="4"
+          value={content}
+          onChange={e => setContent(e.target.value)}
+        />
+      </Grid>
 
-    <Button variant="contained" color="primary" name="submit" onClick={() => dispatch(actions.saveReview(id, {name, content}))}>
-      Submit
-    </Button>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" name="submit" onClick={() => dispatch(actions.saveReview(id, {name, content}))}>
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
   </form>)
 }
 
