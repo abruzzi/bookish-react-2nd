8f58fe288c8348b60b10396c9919ea955e6f88ad
Juntao Qiu <juntao.qiu@gmail.com>
2019-09-29 21:15:27 +1000

    setstate

--- src/BookDetail/BookDetail.jsx
+++ src/BookDetail/BookDetail.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import ReviewList from './ReviewList';
 import TextField from "@material-ui/core/TextField/TextField";
 import Button from "@material-ui/core/Button/Button";
 
 const BookDetail = ({book}) => {
+  const [name, setName] = useState('');
+  const [content, setContent] = useState('');
+
   return (<div className="detail">
     <h2 className="book-title">{book.name}</h2>
     <p className="book-description">{book.description}</p>
@@ -14,6 +17,8 @@ const BookDetail = ({book}) => {
         name="name"
         margin="normal"
         variant="outlined"
+        value={name}
+        onChange={e => setName(e.target.value)}
       />
 
       <TextField
@@ -23,6 +28,8 @@ const BookDetail = ({book}) => {
         variant="outlined"
         multiline
         rowsMax="4"
+        value={content}
+        onChange={e => setContent(e.target.value)}
       />
 
       <Button variant="contained" color="primary" name="submit">
